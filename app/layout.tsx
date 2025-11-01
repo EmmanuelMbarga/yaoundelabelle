f46f10a720e02c647693f013211711cc208dc7f6
djohclaude <claudedjoh9@gmail.com>
2025-11-01 16:21:34 +0100

    validation2

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className={geistSans.variable{geistMono.variable}}>
-      
+      <head>
+                // lien d'analyse GOOGLE-ANALYTIC
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PRXB4F4S0G"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PRXB4F4S0G');
+        </script>
+        </head>
       <body>
          {/* Fallback recommandé par Google */}
         <noscript>
